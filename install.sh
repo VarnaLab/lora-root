5160a7d8c8f74e620c8f46103752c554e68b8776
Todor Dragnev <todor@bstconsult.com>
2018-03-25 22:33:09 +0300

    install instruction and script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# exclude these files from creating links
+EXCLUDE="^.git|^README|^INSTALL|^janus"
+
+usage() {
+  echo "Please run this script from 'lora-root' directory!"
+  echo "Usage:  ./bin/install.sh"
+}
+
+pwd=`pwd`
+if [ `basename ${pwd}` != 'lora-root' ] ; then
+  usage
+fi
+
+# update submodules (vim-janus)
+git submodule update --init
+
+#for i in `find . -depth 1 -exec basename {} \;`; do
+for i in `find . -maxdepth 1 -exec basename {} \;`; do
+  echo ${i} | egrep ${EXCLUDE} 1> /dev/null && echo "skipping ${i}" &&  continue
+  if [ -f ${HOME}/${i} ] ; then
+    echo "${HOME}/$i already exists"
+    continue
+  fi
+  if [ -d ${HOME}/${i} ] ; then
+    echo "${HOME}/$i already exists"
+    continue
+  fi
+  ln -vs ${pwd}/$i ${HOME}/$i
+done
